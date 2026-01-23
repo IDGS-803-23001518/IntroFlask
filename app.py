e2030d5adcbf4d917ff9b691d9c36ab5b3753179
IDGS-803-23001518 <83288@alumnos.utleon.edu.mx>
2026-01-22 21:07:36 -0600

    Macros

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    titulo="IDGS803-Flask"
+    lista=['Pedro','mario','juan']
+    return render_template("index.html",titulo=titulo, lista=lista)
+
+@app.route("/alumnos")
+def alumnos():
+    return render_template("alumnos.html")
+
+@app.route("/usuario")
+def usuario():
+    return render_template("usuario.html")
 
 @app.route('/hola')
 def hola():
@@ -41,6 +51,35 @@ def operas():
         <input type="text" id="name" name="name" required>
         </form>
     '''
+
+@app.route("/operaBas", methods=["GET", "POST"])
+def operas1():
+
+    resultado=0
+    if request.method == "POST":
+        n1 = float(request.form.get("n1"))
+        n2 = float(request.form.get("n2"))
+        operacion = request.form.get("operacion")
+        if operacion == "suma":
+            resultado = n1 + n2
+            op = "suma"
+        
+        elif operacion == "resta":
+            resultado = n1 - n2
+            op = "resta"
+        
+        elif operacion == "multiplicacion":
+            resultado = n1 * n2
+            op = "multiplicación"
+        
+        elif operacion == "division":
+            if n2 == 0:
+                return "<h1>Error: No se puede dividir entre 0</h1>"
+            resultado = n1 / n2
+            op = "división"
+
+    return render_template("operaBas.html",resultado=resultado)
+
 
 if __name__ == '__main__':
     app.run()
